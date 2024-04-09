05f4ae74a0d4a5da2b915762ff6128d157b3b915
sahil390 <snarang445566@gmail.com>
2024-04-09 20:04:33 +0530

    Update API key in chat.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# Spad
-Spad is basic text editor which make by using python tkinter liberary its light weight and great project for practice purpasis.
--- chat.py
+++ chat.py
@@ -1,7 +1,7 @@
 import openai
 from PyQt5 import QtWidgets, QtGui
 
-openai.api_key = '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'
+openai.api_key = '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'
 
 class ChatWindow(QtWidgets.QWidget):
     def __init__(self):
